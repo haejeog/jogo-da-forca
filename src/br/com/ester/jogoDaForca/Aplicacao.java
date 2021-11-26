acb441b1f612dd0608a1cba616b6107a15f863fd
Ronaldo <matheus.dominguez@outlook.com>
2021-11-26 16:48:05 -0300

    Funções básicas adicionadas

--- src/br/com/ester/jogoDaForca/Aplicacao.java
+++ src/br/com/ester/jogoDaForca/Aplicacao.java
@@ -3,9 +3,22 @@ package br.com.ester.jogoDaForca;
 import br.com.ester.jogoDaForca.modelo.Jogo;
 
 public class Aplicacao {
-
 	public static void main(String[] args) {
+		boolean jogar = true, jogadas = true;
+		int cont = 0;
 		Jogo p = new Jogo();
-		System.out.println(p.getPalavra());
+		while(jogar){
+			jogadas = p.selecionarLetra();
+		if(jogadas == false){
+			
+			cont++;
+			System.out.println("\nRestam somente "+ (3-cont)+ " tentativas");
+		}
+		if(cont==3){
+			System.out.println("\nA palavra era: "+p.getPalavra());
+			System.out.println("\nVocê perdeu!");
+			jogar = false;
+		}
+		}
 	}
 }
--- src/br/com/ester/jogoDaForca/modelo/Jogo.java
+++ src/br/com/ester/jogoDaForca/modelo/Jogo.java
@@ -1,17 +1,25 @@
 package br.com.ester.jogoDaForca.modelo;
 
-public class Jogo{
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Jogo {
 
 	private String palavra;
 	private Palavras palavras = new Palavras();
-
+	private Scanner sc = new Scanner(System.in);
+	private String teste[];
+	private static int count = 0;
 	
-	public Jogo(){
-		
+
+	public Jogo() {
+
 		this.palavra = palavras.palavraAleatoria();
 		iniciarJogo();
+		
 	}
-	
+
 	public String getPalavra() {
 		return palavra;
 	}
@@ -19,16 +27,46 @@ public class Jogo{
 	public void setPalavra() {
 		this.palavra = palavras.palavraAleatoria();
 	}
-	
-	public void iniciarJogo(){
+
+	public void iniciarJogo() {
+		String teste[] = new String[palavra.length()];
 		for (int i = 0; i < palavra.length(); i++) {
 			System.out.print("- ");
-			
+			teste[i]= "- ";
+
 		}
+		this.teste = teste;
+
 	}
-	public void selecionarLetra(String letra){
+
+	public boolean selecionarLetra() {
+		String[] palavraAuxiliar= palavra.split("");
+		String letra;
+		List<String> palavras = new ArrayList<>();
+		boolean verificarLetra = false,verificarPalavra=false;
 		
+		System.out.println("\nDigite uma letra:");
+		letra = sc.next();
+		for (int i = 0; i < teste.length; i++) {
+           if(palavraAuxiliar[i].equalsIgnoreCase(letra)){
+        	   teste[i]=palavraAuxiliar[i];
+        	   verificarLetra = true;
+           }
+		}
+		int contador = 0;
+		for (String strin : teste) {
+		
+		if(!strin.contentEquals("- ")){
+			contador++;
+		}
+			System.out.print(strin+ " ");
+			if(contador== palavra.length()){
+				System.out.println("\nParabéns, você venceu!!!");
+			System.exit(0);
+			}
+		}
+		
+		return verificarLetra;
 	}
-	
-	
+
 }
